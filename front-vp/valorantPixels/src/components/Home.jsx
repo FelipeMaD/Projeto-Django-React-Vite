d202133941e28fd11a01033ae46b6a964993a4e2
FelipeMaD <felipemarinhodr@hotmail.com>
2024-11-03 11:46:04 -0300

    update home + criação de links

--- front-vp/valorantPixels/src/components/Home.jsx
+++ front-vp/valorantPixels/src/components/Home.jsx
@@ -1,13 +1,50 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Home.css'
+import logo from '../assets/logo.png'
 
 function Home(){
     return(
         <div>
-            <h1>Olá, Mundo</h1>
-            <Link to="/pixels/create" className="create-button">Criar Novo Pixel</Link>
-            <Link to="/pixels/" className="create-button">Listar Pixels</Link>
+            <header className='header'>
+                <div className='left-header'>
+                    <Link to="/Home" className='home'><img src={logo} alt="" className='imagem' /></Link>
+                    <a href="#"><p>Home</p></a>
+                    <a href="#"><p>Sobre nós</p></a>
+                </div>
+                <div className='right-header'>
+                    <a href="#"><p>Suporte</p></a>
+                </div>
+            </header>
+            <nav className='navbar'>
+                <div className='links-navbar'>
+                    <Link to="#">agentes</Link>
+                    <Link to="#">mapas</Link>
+                    <Link to="#">top ajudantes</Link>
+                    <Link to="#">comunidade</Link>
+                </div>
+                <div>
+                    <div></div>
+                    <Link to="#">Hi, joao</Link>
+                </div>
+            </nav>
+            <main className='main'>
+                <div className="Main-content-area">
+                    <div>
+                        <Link to="/pixels/create" className="create-button">Criar Novo Pixel</Link>
+                    </div>
+                    
+                    <img className='logo-middle' src={logo} alt="" />
+                    
+                    <div>
+                        <Link to="/pixels/" className="create-button">Listar Pixels</Link>
+                    </div>
+                    
+                </div>
+               
+            </main>
+            
+            
         </div>
     )
 }
